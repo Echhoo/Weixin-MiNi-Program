abafca6b422fe8ecb9d0268c6ca5f044803f7ce4
Camel-Prince <1784831621@qq.com>
2021-05-27 21:52:38 +0800

    上传功能部分实现

--- FanYou/miniprogram/pages/upload/upload.js
+++ FanYou/miniprogram/pages/upload/upload.js
@@ -1,52 +1,39 @@
 Page({
   data: {
-      file: "",
+      fileID: "",
   },
   onLoad() {
-      this.setData({
-          selectFile: this.selectFile.bind(this),
-          uplaodFile: this.uplaodFile.bind(this)
-      })
+      
   },
-  chooseImage: function (e) {
+  uploadViewData(res){
+    console.log(res.detail.value)
+  },
+  chooseAndUploadImage: function (e) {
       var that = this;
       wx.chooseImage({
           count: 1,
           sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
           sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-          success: function (res) {
-            console.log("RRRRRRR: ",res)
-              // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
-              that.setData({
-                  file: res.tempFilePaths
-              });
-              console.log("temp filse: ",res.tempFilePaths)
-          }
       })
-  },
-  previewImage: function(e){
-      wx.previewImage({
-          current: e.currentTarget.id, // 当前显示图片的http链接
-          urls: this.data.filse // 需要预览的图片http链接列表
+      .then(res=>{
+        console.log("temp file: ",res.tempFilePaths[0])
+        const filePath = res.tempFilePaths[0];
+        const cloudPath = 'upload/city_img' + filePath.match(/\.[^.]+?$/)[0];
+        console.log("cloudPath: ",cloudPath)
+        wx.cloud.uploadFile({
+          cloudPath: cloudPath,
+          filePath: filePath, // 文件路径
+        }).then(res => {
+          // get resource ID
+          console.log(res.fileID)
+          this.setData({
+              fileID: res.fileID
+          })
+        }).catch(error => {
+          // handle error
+          console.log("up err", error)
+        })
+    
       })
-  },
-  selectFile(filse) {
-      console.log('filse', filse)
-      // 返回false可以阻止某次文件上传
-  },
-  uplaodFile(filse) {
-      console.log('upload filse', filse)
-      // 文件上传的函数，返回一个promise
-      return new Promise((resolve, reject) => {
-          setTimeout(() => {
-              reject('some error')
-          }, 1000)
-      })
-  },
-  uploadError(e) {
-      console.log('upload error', e.detail)
-  },
-  uploadSuccess(e) {
-      console.log('upload success', e.detail)
   }
 });
