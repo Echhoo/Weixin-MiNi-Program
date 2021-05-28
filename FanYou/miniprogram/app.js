172463c33411d520dfb72dd2bf765395d07d33a1
Lexie <mlx000127@163.com>
2021-05-29 01:57:41 +0800

    liuyin - modify animation without some

--- FanYou/miniprogram/app.js
+++ FanYou/miniprogram/app.js
@@ -13,5 +13,35 @@ App({
 
     this.globalData = {}
 
+  },
+  show : function(that,param,opacity){
+    var animation = wx.createAnimation({
+      //持续时间800ms
+      duration: 800,
+      timingFunction: 'ease',
+    });
+    //var animation = this.animation
+    animation.opacity(opacity).step()
+    //将param转换为key
+    var json = '{"' + param + '":""}'
+    json = JSON.parse(json);
+    json[param] = animation.export()
+    //设置动画
+    that.setData(json)
+  },
+
+  //滑动渐入渐出
+  slideupshow:function(that,param,px,opacity){
+    var animation = wx.createAnimation({
+      duration: 800,
+      timingFunction: 'ease',
+    });
+    animation.translateY(px).opacity(opacity).step()
+    //将param转换为key
+    var json = '{"' + param + '":""}'
+    json = JSON.parse(json);
+    json[param] = animation.export()
+    //设置动画
+    that.setData(json)
   }
 })
