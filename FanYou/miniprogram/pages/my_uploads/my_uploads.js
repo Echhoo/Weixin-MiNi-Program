c3778ba490fee882c120e95b5fdc21c6d04c0a48
Camel-Prince <1784831621@qq.com>
2021-05-28 16:59:09 +0800

    实先上传功能

---
+++ FanYou/miniprogram/pages/my_uploads/my_uploads.js
@@ -0,0 +1,139 @@
+const db = wx.cloud.database();
+const _ = db.command;
+let fes_name_list = ["清明", "五一", "端午", "儿童节", "中秋", "七夕", "国庆", "春节", "元宵"]
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    festivalViewList: [],
+    loading: true,
+    currentView: {},
+    viewData: [],
+    currentTab: 0,
+    // cityViewIDList: [],
+    festivalViewIDList: [],
+    OpenID: '',
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    wx.cloud.callFunction({
+      name:'getOPENID'
+    }).then(res=>{
+      this.setData({
+        OpenID: res.result.openid
+      })
+      console.log("opind:", this.data.OpenID)
+      this.loadMrysData();
+    })
+    var that = this
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          clientHeight: res.windowHeight 
+        });
+      }
+    })
+  },
+  onShow: function(){
+    // this.loadMrysData();
+  },
+  /**
+   * 获取列表数据
+   */
+  loadMrysData: function() {
+    if (!this.data.loading) {
+      return;
+    }
+    // let { pageIndex, pageSize } = this.data;
+    // 根据openid，获取viewIDList,并查询attractions中的数据
+      //获取待审核的数据
+      db.collection("pending_attractions")
+      .where({
+        '_openid': this.data.OpenID
+      })
+      .get()
+      .then(res=>{
+        wx.stopPullDownRefresh();
+          this.setData({
+            festivalViewList: res.data,
+          })
+          console.log(res.data)
+          //改造festivalViewList数据的fes_pic和fes_intro
+          var i = 0;
+          var len = this.data.festivalViewList.length;
+          var views = []
+          for (i; i < len; i++) {
+            var aCurrentFesView = this.data.festivalViewList[i];
+            var j = 0
+            console.log("a current view; ", aCurrentFesView)
+            for(j; j<9; j++){
+              if(aCurrentFesView.festival[j] == true){
+                aCurrentFesView.fes_pic = this.data.festivalViewList[i].fes_pic[j]
+                aCurrentFesView.fes_intro = this.data.festivalViewList[i].fes_intro[j]
+                aCurrentFesView['Festival'] = fes_name_list[j]
+              }
+            }
+            views[i] = aCurrentFesView;
+          }
+          this.setData({
+            festivalViewList: views
+          })
+          console.log("changed view: ", this.data.festivalViewList)
+      })
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+    this.setData({
+      pageIndex: 1,
+      loading: true
+    })
+    this.loadMrysData();
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+    let pageIndex = this.data.pageIndex + 1;
+    this.setData({
+      pageIndex: pageIndex
+    })
+    this.loadMrysData();
+  },
+
+  onTapNavigateTo(e){
+    // console.log("EEEE: ", e)
+    console.log("pending ID",e.currentTarget.dataset.item._id)
+    // wx.navigateTo({
+    //   url: '../check/check?pendingViewID='+e.currentTarget.dataset.item._id
+    // })
+  },
+
+  swichNav: function (e) {
+    var that = this; 
+    if (this.data.currentTab === e.target.dataset.current) {
+    return false; 
+    } else { 
+      that.setData({
+        currentTab: e.target.dataset.current,  
+    })    
+    }
+    // console.log("currentTab: ", this.data.currentTab)    
+    },    
+    swiperChange: function (e) {    
+    console.log(e);    
+    this.setData({  
+    currentTab: e.detail.current,  
+    })
+    },
+
+})
+
--- FanYou/miniprogram/pages/upload/upload.js
+++ FanYou/miniprogram/pages/upload/upload.js
@@ -1,39 +1,200 @@
+var db = wx.cloud.database();
 Page({
   data: {
-      fileID: "",
+     submited: false,
+      files: [],
+      img_urls: [],
+      OpenID: '',
+      fes_array: ['清明', '五一', '端午', '儿童节', '中秋', '七夕', '国庆', '春节', '元宵'],
+      fes_index: 0,
+  },
+  bindPickerChange: function(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      fes_index: e.detail.value
+    })
   },
   onLoad() {
-      
-  },
-  uploadViewData(res){
-    console.log(res.detail.value)
-  },
-  chooseAndUploadImage: function (e) {
-      var that = this;
-      wx.chooseImage({
-          count: 1,
-          sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-          sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-      })
-      .then(res=>{
-        console.log("temp file: ",res.tempFilePaths[0])
-        const filePath = res.tempFilePaths[0];
-        const cloudPath = 'upload/city_img' + filePath.match(/\.[^.]+?$/)[0];
-        console.log("cloudPath: ",cloudPath)
-        wx.cloud.uploadFile({
-          cloudPath: cloudPath,
-          filePath: filePath, // 文件路径
-        }).then(res => {
-          // get resource ID
-          console.log(res.fileID)
-          this.setData({
-              fileID: res.fileID
-          })
-        }).catch(error => {
-          // handle error
-          console.log("up err", error)
+      wx.cloud.callFunction({
+        name: "getOPENID"
+      }).then(res=>{
+        this.setData({
+          OpenID: res.result.event.openid
         })
-    
       })
-  }
+      this.setData({
+        selectFile: this.selectFile.bind(this),
+        uplaodFile: this.uplaodFile.bind(this)
+    })
+  },
+  reset(){
+    this.setData({
+      submited: false
+    })
+  },
+  submitViewData(res){
+    var data = res.detail.value;
+    console.log(res.detail.value)
+    var view = {}
+    view['site_name'] = data.site_name;
+    view['city'] = data.city;
+    view['fes_intro'] = ["","","","","","","","",""];
+    view['fes_intro'][this.data.fes_index] = data.intro
+    // view['introduction'] = data.intro;
+    view['festival'] = [false,false,false,false,false,false,false,false,false];
+    view['festival'][this.data.fes_index] = true;
+    view['img_url'] = this.data.img_urls[0];
+    view['fes_pic'] = ["","","","","","","","",""]
+    view['fes_pic'][this.data.fes_index] = this.data.img_urls[1];
+    // check_status 有checking，accepted 和 rejected
+    view['check_status'] =  "checking"
+    console.log("new View: ",view)
+    db.collection("pending_attractions").add({
+      data: view
+    })
+    .then(res=>{
+      console.log("add res: ", res)
+    })
+    this.setData({
+      submited: true
+    })
+  },
+  // chooseAndUploadImage1: function (e) {
+  //     var that = this;
+  //     wx.chooseImage({
+  //         count: 1,
+  //         sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+  //         sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+  //     })
+  //     .then(res=>{
+  //       console.log("temp file: ",res.tempFilePaths[0])
+  //       const filePath = res.tempFilePaths[0];
+  //       const cloudPath = 'upload/'+this.data.OpenID + filePath.match(/\.[^.]+?$/)[0];
+  //       console.log("cloudPath: ",cloudPath)
+  //       wx.cloud.uploadFile({
+  //         cloudPath: cloudPath,
+  //         filePath: filePath, // 文件路径
+  //       }).then(res => {
+  //         // get resource ID
+  //         console.log(res.fileID)
+  //         this.setData({
+  //             img_url: res.fileID
+  //         })
+  //       }).catch(error => {
+  //         // handle error
+  //         console.log("up err", error)
+  //       })
+    
+  //     })
+  // },
+  // chooseAndUploadImage2: function (e) {
+  //   var that = this;
+  //   wx.chooseImage({
+  //       count: 1,
+  //       sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+  //       sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+  //   })
+  //   .then(res=>{
+  //     console.log("temp file: ",res.tempFilePaths[0])
+  //     const filePath = res.tempFilePaths[0];
+  //     const cloudPath = 'upload/'+this.data.OpenID+"-1" + filePath.match(/\.[^.]+?$/)[0];
+  //     console.log("cloudPath: ",cloudPath)
+  //     wx.cloud.uploadFile({
+  //       cloudPath: cloudPath,
+  //       filePath: filePath, // 文件路径
+  //     }).then(res => {
+  //       // get resource ID
+  //       console.log(res.fileID)
+  //       this.setData({
+  //           a_fes_pic: res.fileID
+  //       })
+  //     }).catch(error => {
+  //       // handle error
+  //       console.log("up err", error)
+  //     })
+  
+  //   })
+  // },
+  chooseImage: function (e) {
+    var that = this;
+    wx.chooseImage({
+        sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+        sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+        success: function (res) {
+          console.log("res", res)
+            // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+            that.setData({
+                files: that.data.files.concat(res.tempFilePaths)
+            });
+        }
+    })
+},
+previewImage: function(e){
+    wx.previewImage({
+        current: e.currentTarget.id, // 当前显示图片的http链接
+        urls: this.data.files // 需要预览的图片http链接列表
+    })
+},
+selectFile(files) {
+    console.log('files', files)
+    // 返回false可以阻止某次文件上传
+},
+uplaodFile(files) {
+    console.log('upload files', files)
+    // 文件上传的函数，返回一个promise
+    return new Promise((resolve, reject) => {
+      const db = wx.cloud.database()
+      // console.log('upload files', files);
+      let filesData = files.tempFilePaths;
+      this.setData({
+          arrurls:[]
+      })
+      var object = {};
+      const that = this;
+      for (let i = 0; i < filesData.length; i++) {
+      const filePath = filesData[i];
+      const name = 'upload/'+'upload_img-'+parseInt(Math.random() * 100000000);
+      const cloudPath = name + filePath.match(/\.[^.]+?$/)[0]
+      wx.cloud.uploadFile({
+        cloudPath,
+        filePath,
+        success: res => {
+          // console.log('[上传文件] 成功：', res);
+          that.setData({
+            arrurls: that.data.arrurls.concat(res.fileID)
+          });
+          object['urls'] = that.data.arrurls;
+        
+          if(that.data.arrurls.length == filesData.length){
+            console.log(object);
+            resolve(object)
+          }
+        //   _this.setData({
+        //     upimgData: _this.data.upimgData.concat(res.fileID)
+        //   })
+        },
+        fail: res => {
+          console.error('[上传文件] 失败：', res)
+        //   wx.showToast({
+        //     icon: 'none',
+        //     title: '上传失败',
+        //   })
+        },
+        complete: () => {
+          wx.hideLoading()
+        }
+      })
+    }
+    })
+},
+uploadError(e) {
+    console.log('upload error', e.detail)
+},
+uploadSuccess(e) {
+  console.log('upload success', e.detail.urls)
+  this.setData({
+    img_urls: e.detail.urls
+  })
+  console.log("img urls: ", this.data.img_urls)
+}
 });
